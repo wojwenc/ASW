5012a685a84007071b68dcfc60a9358b0db957b4
Your Name <you@example.com>
2021-01-20 09:43:04 +0100

    Lab 2

---
+++ Lab2/multiRecvBME280.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3.6
+import paho.mqtt.client as mqtt
+import threading
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("sensors/bme280/#")
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.payload)
+    if 'temp' in msg.topic:
+        print(msg.payload)
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect("192.168.51.239", 1883, 60)
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect_local(client_local, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client_local.subscribe("sensors/bme280/#")
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message_local(client_local, userdata, msg):
+    if 'temp' in msg.topic:
+        print(msg.payload)
+
+client_local = mqtt.Client()
+client_local.on_connect = on_connect_local
+client_local.on_message = on_message_local
+client_local.connect("localhost", 1883, 60)
+
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+
+threading.Thread(target=client_local.loop_forever).start()
+
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+threading.Thread(target=client.loop_forever).start()
---
+++ Lab2/sensorBME280.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3.6
+import time
+import paho.mqtt.client as mqtt
+import threading
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect_local(client_local, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client_local.subscribe("gpio/#")
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message_local(client_local, userdata, msg):
+    value = msg.payload.decode('utf-8')
+    if value == '1':
+        path = "/sys/bus/iio/devices/iio:device0/in_temp_input"
+        with open(path,'r',encoding = 'utf-8') as f:
+            client_local.publish("sensors/bme280/temp", str(f.read()))
+
+client_local = mqtt.Client()
+client_local.on_connect = on_connect_local
+client_local.on_message = on_message_local
+client_local.connect("localhost", 1883, 60)
+
+
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+
+threading.Thread(target=client_local.loop_forever).start()
+
+while(True):
+    path = "/sys/bus/iio/devices/iio:device0/in_pressure_input"
+    with open(path,'r',encoding = 'utf-8') as f:
+        client_local.publish("sensors/bme280/pres", str(f.read()))
+    path = "/sys/bus/iio/devices/iio:device0/in_humidityrelative_input"
+    with open(path,'r',encoding = 'utf-8') as f:
+        client_local.publish("sensors/bme280/hum", str(f.read()))
+    time.sleep(5)
